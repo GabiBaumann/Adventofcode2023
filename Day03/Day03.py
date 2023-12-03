45f821dc3cbcb797bb278112282f73c1a6175179
GabiBaumann <andre.mueller@gesis.org>
2023-12-03 23:54:36 +0100

    Day 03: if has symbol, don't keep looking.
    Changes to be committed:
    	modified:   Day03.py

--- Day03/Day03.py
+++ Day03/Day03.py
@@ -126,8 +126,9 @@ for y, line in enumerate(es):
                         if es[i][j] not in no_symbol: has_symbol = True
             else: 
                 pn += char
-                for i in range(max(0,y-1), min(my, y+2)):
-                    if es[i][x+1] not in no_symbol: has_symbol = True
+                if not has_symbol:
+                    for i in range(max(0,y-1), min(my, y+2)):
+                        if es[i][x+1] not in no_symbol: has_symbol = True
         elif char == '.' or char == '\n':
             if in_num:
                 in_num = False
